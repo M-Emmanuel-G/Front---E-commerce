66d1c677dfe6985a57da811ecb4795d0e603d5c1
Marcio Emmanuel <100731829+M-Emmanuel-G@users.noreply.github.com>
2023-10-30 21:35:27 -0300

    Update URL.js

--- src/URL/URL.js
+++ src/URL/URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3003'
-// export const BASE_URL = 'https://ecommerce-backend-dpon.onrender.com'
+// export const BASE_URL = 'http://localhost:3003'
+export const BASE_URL = 'https://ecommerce-backend-dpon.onrender.com'
